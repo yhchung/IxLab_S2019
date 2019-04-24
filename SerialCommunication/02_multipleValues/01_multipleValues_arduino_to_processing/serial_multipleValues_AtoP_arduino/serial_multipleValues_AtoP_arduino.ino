db867bcabcff1af7f7d434d3a5345cb9e99f70e1
Young Chung <younghyun.chung@gmail.com>
2019-04-24 23:17:52 +0800

    class 22

--- SerialCommunication/02_multipleValues/01_multipleValues_arduino_to_processing/serial_multipleValues_AtoP_arduino/serial_multipleValues_AtoP_arduino.ino
+++ SerialCommunication/02_multipleValues/01_multipleValues_arduino_to_processing/serial_multipleValues_AtoP_arduino/serial_multipleValues_AtoP_arduino.ino
@@ -9,13 +9,14 @@ void setup() {
 
 void loop() {
   int sensor1 = analogRead(A0);
-  int sensor2 = analogRead(A1);
+  //int sensor2 = analogRead(A1);
   //int sensor3 = analogRead(A2);
 
   // keep this format
   Serial.print(sensor1);
-  Serial.print(",");  // put comma between sensor values
-  Serial.println(sensor2);
+  Serial.println();
+//  Serial.print(",");  // put comma between sensor values
+//  Serial.println(sensor2);
 //  Serial.print(",");
 //  Serial.print(sensor3);
 //  Serial.println(); // add linefeed after sending the last sensor value
